108d0a188af22fe288653b1d7e40383a943caad4
vincent caldwell <vcaldwel8@yahoo.com>
2020-09-21 18:07:42 -0400

    created html, css and script files. started Basics section

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//Change the "Are you ready?" to "Let's Go!" when button is clicked
+function changeText() {
+    document.getElementById("ready-text").innerHTML = "Let's Go!";
+    document.getElementById("ready-text").style.fontSize = "50px";
+}
+
+function helloWorld() {
+    alert("Hello World!");
+}
+
+
+// function calculate(numbOne, numbTwo, operator) {
+//     console.log(operator);
+//     if(operator == "add_operator"){
+//         let sum = Number(numbOne.value) + Number(numbTwo.value);
+//     document.getElementById("total_text").innerHTML = sum;
+//     }
+    
+// }
