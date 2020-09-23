91ef5e7617cd656e82c807a0fc937ea54f3577ef
vincent caldwell <vcaldwel8@yahoo.com>
2020-09-22 20:54:58 -0400

    adding some javascript functions in notes to be used later

--- script.js
+++ script.js
@@ -18,3 +18,46 @@ function helloWorld() {
 //     }
     
 // }
+
+// _______________________________________
+//REDUCE function
+// function aVeryBigSum(ar) {
+//     return ar.reduce(totalArray);
+
+//     function totalArray (total, num){
+//         return total + num;
+//     }
+
+// }
+
+//___________________________________________
+//MULTI-DIMENSIONAL ARRAY function
+// function diagonalDifference(arr) {
+//     // Write your code here
+//     let leftToRightTotal = 0;
+//     let rightToLeftTotal = 0;
+
+//     function leftDiagonal(array) {
+//         let tempTotal= 0;
+//         for (let i = 0; i < array.length; i++){
+//             tempTotal += array[i][i];
+//         }
+//         leftToRightTotal += tempTotal;
+//     }
+
+//     function rightDiagonal(array) {
+//         let tempTotal = 0;
+//         let arrLength = ((array.length) -1);
+//         for (let i = (arrLength); i >= 0; i--){
+//             tempTotal += array[(Math.abs(i-arrLength))][i];
+//         }
+//         rightToLeftTotal += tempTotal;
+//     }
+
+
+//     leftDiagonal(arr);
+//     rightDiagonal(arr);
+//     return (Math.abs(leftToRightTotal- rightToLeftTotal));
+//     //console.log(rightToLeftTotal);
+//     //console.log(leftToRightTotal);
+// }
