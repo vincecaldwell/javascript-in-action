80b8e38ff5cb1f06a7a07f02f6387f2bd291fe86
vincent caldwell <vcaldwel8@yahoo.com>
2020-09-24 21:13:24 -0400

    started working on box project, finished random background color generator

--- script.js
+++ script.js
@@ -13,11 +13,8 @@ function helloWorld() {
 //Calculator function
 function calculate(numbOne, numbTwo) {
     let operatorSign = document.querySelector('[name="operator"]:checked').value;
-    //console.log(operatorSign);
     let num1= Number(numbOne.value);
-    //console.log(num1);
     let num2= Number(numbTwo.value);
-    //console.log(num2);
     let total;
 
     switch (operatorSign) {
@@ -36,11 +33,23 @@ function calculate(numbOne, numbTwo) {
         default:
             break;
     }
+    document.getElementById("total_text").innerHTML = "Total: " + total;   
+}
 
+//Change Color function
+function changeColor() {
+    let colorChangeBox = document.getElementById("div_color_change_box");
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    colorChangeBox.style.backgroundColor = randomColor;
+}
 
-    document.getElementById("total_text").innerHTML = "Total: " + total;
+//Change Shape Function
+function changeShape() {
+    let shapeChangeBox = document.getElementById("div_shape_change_box");
+
     
 }
+
 
 // _______________________________________
 //REDUCE function
@@ -84,3 +93,37 @@ function calculate(numbOne, numbTwo) {
 //     //console.log(rightToLeftTotal);
 //     //console.log(leftToRightTotal);
 // }
+
+//MAP FUNCTION in action
+// function gradingStudents(grades) {
+//     // Write your code here
+
+//     function roundGradeOrNot(grade){
+//         let nextMultipleOfFive = (grade-(grade%5))+5;
+//         //console.log(nextMultipleOfFive);
+//         if((nextMultipleOfFive - grade) < 3 && grade >= 38){
+//             return nextMultipleOfFive;
+//         } else{
+//             return grade;
+//             }
+//     }
+//     return grades.map(roundGradeOrNot);
+// }
+
+
+// _______________________________________
+// const googleDatabase = [
+//     'cats.com',
+//     'dogs.com',
+//     'food.com'
+// ];
+
+
+// const googleSearch = (searchInput, db) => {
+//     const matches= googleDatabase.filter(website => {
+//         return website.includes(searchInput);
+//     })
+//     return matches.length > 3 ? matches.slice(0,3) : matches;
+// }
+
+// console.log(googleSearch("cats", googleDatabase));
